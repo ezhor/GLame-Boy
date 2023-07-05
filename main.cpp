7105e487d37c3ac6a122a779c77747a3c6ebcbb6
Ézhor Malkávar <ezhormalkavar@gmail.com>
2023-07-05 14:46:57 +0200

    Rectangle render

--- main.cpp
+++ main.cpp
@@ -13,18 +13,37 @@ const char* vertexShaderSource =
 "gl_Position = vec4(position.x, position.y, position.z, 1.0);\n"
 "}\0";
 
-const char* fragmentShaderSource =
+const char* blueFragmentShaderSource =
 "#version 330 core\n"
 "out vec4 FragmentColor;\n"
 "void main()\n"
 "{\n"
 "FragmentColor = vec4(0.0f, 0.0f, 0.5f, 1.0f);"
-"}\0"
-;
+"}\0";
+
+const char* greenFragmentShaderSource =
+"#version 330 core\n"
+"out vec4 FragmentColor;\n"
+"void main()\n"
+"{\n"
+"FragmentColor = vec4(0.0f, 0.5f, 0.0f, 1.0f);"
+"}\0";
+
+bool polygonMode = true;
+bool pWassPressed = false;
 
 void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
+	}
+}
+
+void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_P && action == GLFW_PRESS)
+	{
+		polygonMode = !polygonMode;
+		glPolygonMode(GL_FRONT_AND_BACK, polygonMode ? GL_FILL : GL_LINE);
 	}
 }
 
@@ -44,6 +63,7 @@ GLFWwindow* initWindow() {
 		std::cout << "Failet to initialize GLAD" << std::endl;
 	}
 
+	glfwSetKeyCallback(window, keyCallback);
 	glClearColor(0.5f, 0.0f, 0.0f, 1.0f);
 	glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
 
@@ -79,7 +99,7 @@ void checkShaderProgramLinkingError(int shaderProgram) {
 	}
 }
 
-unsigned int compileShaderProgram() {
+unsigned int compileShaderProgram(const char* fragmentShaderSource) {
 	unsigned int vertexShader = compileShader(vertexShaderSource, GL_VERTEX_SHADER);
 	unsigned int fragmentShader = compileShader(fragmentShaderSource, GL_FRAGMENT_SHADER);
 	unsigned int shaderProgram = glCreateProgram();
@@ -93,46 +113,84 @@ unsigned int compileShaderProgram() {
 	return shaderProgram;
 }
 
-unsigned int createVBO() {
-	unsigned int VBO;
-
-	glGenBuffers(1, &VBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-
-	return VBO;
-}
-
-unsigned int createVAO() {
+unsigned int buildTriangle() {
 	float vertices[] = {
-		-0.5f, -0.5f, 0.0f,
-		0.5f, -0.5f, 0.0f,
-		0.0f, 0.5f, 0.0f
+		-0.4f, -0.4f, 0.0f,
+		0.4f, -0.4f, 0.0f,
+		0.0f, 0.4f, 0.0f
+	};
+	unsigned int indices[] = {
+		0, 1, 2
 	};
 
-	unsigned int VAO;
+	unsigned int VAO, VBO, EBO;
 	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
-	glBindBuffer(GL_ARRAY_BUFFER, createVBO());
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 
 	return VAO;
 }
 
-void drawTriangle(unsigned int VAO, unsigned int shaderProgram) {
-	glUseProgram(shaderProgram);
+unsigned int buildRectangle() {
+	float vertices[] = {
+		   -0.5f,  0.5f, 0.0f,
+			0.5f,  0.5f, 0.0f,
+			0.5f, -0.5f, 0.0f,
+		   -0.5f, -0.5f, 0.0f
+	};
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0
+	};
+
+	unsigned int VAO, VBO, EBO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
+
 	glBindVertexArray(VAO);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+
+	return VAO;
 }
 
-void renderLoop(GLFWwindow* window, unsigned int VAO, unsigned int shaderProgram) {
+void draw(unsigned int VAO, unsigned int shaderProgram) {
+	glBindVertexArray(VAO);
+	glUseProgram(shaderProgram);
+	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+void renderLoop(GLFWwindow* window, unsigned int VAOs[], unsigned int shaderPrograms[]) {
 
 	while (!glfwWindowShouldClose(window)) {
 		processInput(window);
 
 		glClear(GL_COLOR_BUFFER_BIT);
-		drawTriangle(VAO, shaderProgram);
+		int size = sizeof(VAOs) / sizeof(VAOs[0]);
+		for (int i = 0; i < size; i++) {
+			draw(VAOs[i], shaderPrograms[i]);
+		}
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -142,9 +200,15 @@ void renderLoop(GLFWwindow* window, unsigned int VAO, unsigned int shaderProgram
 int main()
 {
 	GLFWwindow* window = initWindow();
-	unsigned int shaderProgram = compileShaderProgram();
-	unsigned int VAO = createVAO();
-	renderLoop(window, VAO, shaderProgram);
+	unsigned int VAOs[]{
+		buildRectangle(),
+		buildTriangle()
+	};
+	unsigned int shaderPrograms[]{
+		compileShaderProgram(greenFragmentShaderSource),
+		compileShaderProgram(blueFragmentShaderSource)
+	};
+	renderLoop(window, VAOs, shaderPrograms);
 	glfwTerminate();
 	return 0;
 }
