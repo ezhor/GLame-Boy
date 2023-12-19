8a82e743ef82526d5ee093fdc9de9018549d8f77
Ézhor Malkávar <ezhormalkavar@gmail.com>
2023-12-19 13:39:33 +0100

    vcpkg is no longer used

--- install-vcpkg.bat
+++
@@ -1,2 +0,0 @@
-git clone https://github.com/Microsoft/vcpkg.git
-start /wait .\vcpkg\bootstrap-vcpkg.bat
--- run.bat
+++ run.bat
@@ -1,6 +1,3 @@
-.\vcpkg.\vcpkg.exe install glad:x64-windows
-.\vcpkg.\vcpkg.exe install glfw3:x64-windows
-
 cmake -S . -B ./build
 cmake --build ./build
 
