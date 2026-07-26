000acfd3a07e549ba2091be3ed439d4da1969cf7
Ézhor Malkávar <ezhormalkavar@gmail.com>
2026-07-26 11:37:35 +0200

    Fixed artifacts path

--- run.bat
+++ run.bat
@@ -2,7 +2,7 @@ git submodule init
 git submodule update
 
 cmake -S . -B ./build
-cmake --build ./build
+cmake --build ./build --config Release
 
-".\build\src\Main\Debug\GLame Boy.exe" .\test-roms\blargg\individual\01-special.gb
+".\build\src\Main\Release\GLame Boy.exe" .\test-roms\blargg\individual\01-special.gb
 pause
